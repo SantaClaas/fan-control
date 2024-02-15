6cb9318f9ac6e836085ae863efe0096b829055e4
Claas <claas@claas.dev>
2024-02-15 01:11:52 +0100

    Add first fetch on client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+import { createResource } from "solid-js";
 import Slider from "./Slider";
 
+async function getCurrentSetPoint() {
+  const response = await fetch("/api/fan/2/setpoint/current");
+
+  if (!response.ok) {
+    return { error: "Failed to fetch setpoint" };
+  }
+
+  const data = await response.json();
+  return data;
+}
+
 function App() {
-  return <Slider />;
+  const [setPointResponse] = createResource(getCurrentSetPoint);
+  return <>{JSON.stringify(setPointResponse())}</>;
 }
 
 export default App;
