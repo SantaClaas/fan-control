f2d59e3a1a57db6e25bd16b883074a0055832d44
Claas <claas@claas.dev>
2024-02-19 01:59:44 +0100

    Implement functionality for speed mode selection

--- client/src/FanControl.jsx
+++ client/src/FanControl.jsx
@@ -7,6 +7,7 @@ import {
 
 const MAX_SET_POINT = 64_000;
 async function getSetPoint() {
+  //TODO error handling
   const response = await fetch("/api/fan/2/setpoint");
 
   if (!response.ok)
@@ -26,6 +27,7 @@ async function updateSetPoint(setPoint) {
     throw new Error("Setpoint out of range");
   }
 
+  //TODO error handling
   const response = await fetch("/api/fan/2/setpoint", {
     method: "PATCH",
     headers: {
@@ -35,6 +37,78 @@ async function updateSetPoint(setPoint) {
   });
 }
 
+const OFF = 0;
+const NIGHT = MAX_SET_POINT * 0.1;
+const DAY = MAX_SET_POINT * 0.25;
+const PARTY = MAX_SET_POINT * 0.5;
+
+/**
+ *
+ * @param {{setPoint: import("solid-js").Accessor<number | undefined>, setSetPoint: import("solid-js").Setter<number | undefined>}} param0
+ * @returns
+ */
+function ModeFanControl({ setPoint, setSetPoint }) {
+  /**
+   * @param {{target: HTMLInputElement}} event
+   */
+  function handleChange(event) {
+    console.debug("Setting setpoint", event.target.value);
+
+    const value = Number(event.target.value);
+    if (Number.isNaN(value)) return;
+    setSetPoint(value);
+  }
+
+  const isDisabled = () => setPoint() === undefined;
+
+  const value = () => setPoint() || 0;
+
+  return (
+    <fieldset disabled={isDisabled()}>
+      <legend>Speed</legend>
+      <label>
+        <input
+          type="radio"
+          name="speed"
+          value={OFF.toString()}
+          onChange={handleChange}
+          checked={value() === OFF}
+        />
+        Off
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="speed"
+          value={NIGHT.toString()}
+          onChange={handleChange}
+          checked={OFF < value() && value() <= NIGHT}
+        />
+        Night
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="speed"
+          value={DAY.toString()}
+          onChange={handleChange}
+          checked={NIGHT < value() && value() <= DAY}
+        />
+        Day
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="speed"
+          value={PARTY.toString()}
+          onChange={handleChange}
+          checked={DAY < value() && value() <= PARTY}
+        />
+        High
+      </label>
+    </fieldset>
+  );
+}
 /**
  * A slider range input based on https://codepen.io/bgebelein/pen/wvYeapy
  * @returns {import("solid-js").JSX.Element}
@@ -58,6 +132,12 @@ function FanControl() {
   let input;
   createEffect(() => {
     let value = setPoint();
+    if (value === undefined || Number.isNaN(value)) return;
+
+    // Update server
+    updateSetPoint(value);
+
+    // Reflect change in input
     if (value && input) input.value = value.toString();
   });
 
@@ -80,7 +160,10 @@ function FanControl() {
    */
   function handleChange(event) {
     if (isDisabled()) return;
-    updateSetPoint(Number(event.target.value));
+
+    const value = Number(event.target.value);
+    if (Number.isNaN(value)) return;
+    setSetPoint(value);
   }
 
   createEffect(() => {
@@ -136,6 +219,7 @@ function FanControl() {
         ref={input}
       />
       <span class="align-middle min-w-[4ch] mx-4">{valueLabel()}</span>
+      <ModeFanControl setPoint={setPoint} setSetPoint={setSetPoint} />
     </>
   );
 }
