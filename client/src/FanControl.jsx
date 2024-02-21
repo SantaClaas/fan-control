7e867c6977117b2366ac1df266e93a1533d8a286
Claas <claas@claas.dev>
2024-02-21 17:18:37 +0100

    Animate fan with JS to better control wind up and wind down

--- client/src/FanControl.jsx
+++ client/src/FanControl.jsx
@@ -3,9 +3,26 @@ import {
   createSignal,
   createResource,
   onCleanup,
+  onMount,
 } from "solid-js";
 
 const MAX_SET_POINT = 64_000;
+
+/**
+ * @param {number} t
+ * @param {number} initial
+ * @param {number} p1
+ * @param {number} p2
+ * @param {number} final
+ */
+function cubicBezier2Point(t, initial, p1, p2, final) {
+  return (
+    (1 - t) ** 3 * initial +
+    3 * (1 - t) ** 2 * t * p1 +
+    3 * (1 - t) * t ** 2 * p2 +
+    t ** 3 * final
+  );
+}
 /**
  *
  * @param {boolean} isDemo
@@ -204,6 +221,7 @@ function debounce(callback, delayMilliseconds = 250) {
 }
 
 /**
+ * Based on https://blog.webdevsimplified.com/2022-03/debounce-vs-throttle/
  * @param {Function} callback
  * @param {number} delayMilliseconds
  * @returns {(...args: any[]) => void}
@@ -337,6 +355,48 @@ function FanControl() {
     throttle(setSetPoint, 250)(value);
   }
 
+  /** @type {import("solid-js").Signal<Animation | undefined>} */
+  const [getAnimation, setAnimation] = createSignal();
+
+  /**
+   * @param {SVGElement} fan
+   */
+  function animateFan(fan) {
+    const keyframes = [
+      // { transform: "rotate(0deg)" },
+      { transform: "rotate(360deg)" },
+    ];
+
+    const timing = {
+      // A minute to esily set RPM by manipulating the playback rate
+      duration: 60_000,
+      iterations: Infinity,
+    };
+
+    const animation = fan.animate(keyframes, timing);
+    setAnimation(animation);
+  }
+
+  createEffect(() => {
+    // const animation = animation();
+    const animation = getAnimation();
+    if (!animation) return;
+
+    // When the animation speed is a minute, the playback rate is the RPM
+    animation.updatePlaybackRate(rpm());
+  });
+
+  /** @type {SVGSVGElement | undefined} */
+  let fan;
+
+  // Animations need to start after the component is mounted and rendered to the DOM
+  // Starting the animation in the ref callback doesn't work as the element is created but not yet rendered
+  onMount(() => {
+    if (!fan) return;
+
+    animateFan(fan);
+  });
+
   return (
     <>
       <main class="grid h-[100dvh] grid-rows-[auto_1fr_auto_auto] gap-4 bg-cyan-950 p-4 text-slate-50">
@@ -357,8 +417,8 @@ function FanControl() {
             viewBox="0 -960 960 960"
             width="24"
             fill="currentColor"
-            style={{ "--rpm": rpm() }}
-            class="h-full w-full animate-[spin_calc(60s/var(--rpm))_linear_infinite] rounded-full fill-cyan-300/30"
+            class="h-full w-full rounded-full fill-cyan-300/30"
+            ref={fan}
           >
             <path d="M424-80q-51 0-77.5-30.5T320-180q0-26 11.5-50.5T367-271q22-14 35.5-36t18.5-47l-12-6q-6-3-11-7l-92 33q-17 6-33 10t-33 4q-63 0-111.5-55T80-536q0-51 30.5-77.5T179-640q26 0 51 11.5t41 35.5q14 22 36 35.5t47 18.5l6-12q3-6 7-11l-33-92q-6-17-10-33t-4-32q0-64 55-112.5T536-880q51 0 77.5 30.5T640-781q0 26-11.5 51T593-689q-22 14-35.5 36T539-606l12 6q6 3 11 7l92-34q17-6 32.5-9.5T719-640q81 0 121 67t40 149q0 51-32 77.5T777-320q-25 0-48.5-11.5T689-367q-14-22-36-35.5T606-421l-6 12q-3 6-7 11l33 92q6 16 10 30.5t4 30.5q1 65-54 115T424-80Zm56-340q25 0 42.5-17.5T540-480q0-25-17.5-42.5T480-540q-25 0-42.5 17.5T420-480q0 25 17.5 42.5T480-420Z" />
           </svg>
